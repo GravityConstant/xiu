d8dd545fe810917f0010ff20ed15113b022efe53
lovebefour <love_befour@163.com>
2019-07-09 09:26:01 +0800

    add esl modified

---
+++ esl/command.go
@@ -0,0 +1,58 @@
+// Copyright 2017 Vallimamod Abdullah. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package esl
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"time"
+)
+
+const timeoutPeriod = 60 * time.Second
+
+var errTimeout = errors.New("Timeout")
+
+type Command struct {
+	Sync bool
+	UId  string
+	App  string
+	Args string
+}
+
+// Serialize formats (serializes) the command as expected by freeswitch.
+func (cmd *Command) Serialize() []byte {
+	var buf bytes.Buffer
+	buf.WriteString(fmt.Sprintf("sendmsg %s\ncall-command: execute\n", cmd.UId))
+	buf.WriteString(fmt.Sprintf("execute-app-name: %s\nexecute-app-arg: %s\n", cmd.App, cmd.Args))
+	if cmd.Sync {
+		buf.WriteString("event-lock: true\n")
+	} else {
+		buf.WriteString("event-lock: false\n")
+	}
+	buf.WriteString("\n\n")
+	return buf.Bytes()
+}
+
+// Execute sends Command cmd over Connection and waits for reply.
+// Returns the command reply event pointer or an error if any.
+func (cmd Command) Execute(con *Connection) (*Event, error) {
+	var (
+		ev  *Event
+		err error
+	)
+	_, err = con.Write(cmd.Serialize())
+	if err != nil {
+		return nil, fmt.Errorf("execute command: con write: %v", err)
+	}
+	select {
+	case err = <-con.err:
+		return nil, err
+	case ev = <-con.cmdReply:
+		return ev, nil
+	case <-time.After(timeoutPeriod):
+		return nil, errTimeout
+	}
+}
